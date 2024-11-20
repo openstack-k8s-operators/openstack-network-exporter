c4fa35e0e300c331cfcfa0713ceba2123c90e40b
Robin Jarry <rjarry@redhat.com>
2024-11-20 14:11:28 +0100

    main: add program entry point
    Implement the main entry point of the exporter.
    
    On startup, it will first check the command line arguments. If -l is
    present, all supported metrics of all collectors (regardless of any
    configuration) are printed and the program exits.
    
    If no arguments are specified, all enabled collectors are added to
    a prometheus registry which is then passed to a promhttp handler
    provided by the prometheus client library.
    
    Implement a thin wrapper for HTTPS and Basic authentication depending
    on the configuration.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>

---
+++ main.go
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 Robin Jarry
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/lib"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/config"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/log"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	help = flag.Bool("h", false, "Display help and exit")
+	list = flag.Bool("l", false, "List all supported metrics and exit")
+)
+
+func main() {
+	flag.Parse()
+	switch {
+	case *help:
+		flag.Usage()
+		os.Exit(0)
+	case *list:
+		for _, c := range collectors.Collectors() {
+			lib.PrintMetrics(c)
+		}
+		os.Exit(0)
+	}
+	if err := config.Parse(); err != nil {
+		// logging not initialized yet, directly write to stderr
+		fmt.Fprintf(os.Stderr, "error: failed to parse config: %s\n", err)
+		os.Exit(1)
+	}
+	if err := log.InitLogging(config.LogLevel()); err != nil {
+		// logging not initialized yet, directly write to stderr
+		fmt.Fprintf(os.Stderr, "error: failed to init log: %s\n", err)
+		os.Exit(1)
+	}
+
+	log.Debugf("initializing collectors")
+
+	registry := prometheus.NewRegistry()
+
+	for _, c := range collectors.Collectors() {
+		if lib.CollectorEnabled(c) {
+			log.Infof("registering %T", c)
+
+			if err := registry.Register(c); err != nil {
+				log.Critf("collector: %s", err)
+				os.Exit(1)
+			}
+		} else {
+			log.Infof("%T not registered, metric set not enabled", c)
+		}
+	}
+
+	handler := promhttp.HandlerFor(
+		registry,
+		promhttp.HandlerOpts{
+			ErrorLog:            log.PrometheusLogger(),
+			ErrorHandling:       promhttp.ContinueOnError,
+			MaxRequestsInFlight: 10,
+			Timeout:             2 * time.Second,
+			EnableOpenMetrics:   true,
+		},
+	)
+	mux := http.NewServeMux()
+	mux.Handle(config.HttpPath(), handler)
+
+	var err error
+	server := http.Server{Addr: config.HttpListen(), Handler: mux, ErrorLog: log.ErrorLogger()}
+
+	if config.TlsCert() != "" && config.TlsKey() != "" {
+		log.Noticef("listening on https://%s%s", config.HttpListen(), config.HttpPath())
+		server.Handler = basicAuthHandler(mux)
+		err = server.ListenAndServeTLS(config.TlsCert(), config.TlsKey())
+	} else {
+		log.Noticef("listening on http://%s%s", config.HttpListen(), config.HttpPath())
+		err = server.ListenAndServe()
+	}
+	if err != nil {
+		log.Critf("listen: %s", err)
+		os.Exit(1)
+	}
+}
+
+func basicAuthHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var user, password, pass string
+		var ok, authorized bool
+
+		if user, pass, ok = r.BasicAuth(); ok {
+			if password, ok = config.AuthUsers()[user]; ok && pass == password {
+				authorized = true
+			}
+		}
+		if authorized {
+			handler.ServeHTTP(w, r)
+		} else {
+			w.Header().Add("WWW-Authenticate", `Basic realm="ovs-node-exporter"`)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		}
+	})
+}
