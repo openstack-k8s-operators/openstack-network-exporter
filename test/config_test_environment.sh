c69e9052acd26e2e98b4600cc3e9d4b05f24fa17
Miguel Angel Nieto Jimenez <mnietoji@redhat.com>
2025-02-17 10:15:25 +0100

    test: add test job to workflow
    Added testing job. It will test that the output of
    openstack-network-exporter output is properly generated
    
    Signed-off-by: Miguel Angel Nieto Jimenez <mnietoji@redhat.com>

---
+++ test/config_test_environment.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+apt-get install openvswitch-switch-dpdk iperf3 socat -y
+update-alternatives --set ovs-vswitchd \
+	/usr/lib/openvswitch-switch-dpdk/ovs-vswitchd-dpdk
+systemctl restart openvswitch-switch
+
+ovs-vsctl set o . other_config:pmd-cpu-mask=0x06
+ovs-vsctl set o . other_config:dpdk-extra="-a 0000:00:00.0 --no-huge --legacy-mem -m 2048"
+ovs-vsctl set o . other_config:dpdk-init=true
+systemctl restart openvswitch-switch
+
+ovs-vsctl add-br br-phy-0 -- set bridge br-phy-0 datapath_type=netdev
+
+for i in 0 1; do
+	tap="dtap$i"
+	ns="ns_$i"
+	ovs-vsctl add-port br-phy-0 "$tap" -- set interface "$tap" type=dpdk \
+		options:dpdk-devargs=net_tap$i,iface=$tap options:n_rxq=3
+	ip netns add "$ns"
+	ip link set "$tap" netns "$ns"
+	ip -n "$ns" link set lo up
+	ip -n "$ns" link set "$tap" up
+	ip -n "$ns" addr add 10.10.10.1$i/24 dev "$tap"
+	ip -n "$ns" addr show
+done
+
+ip netns exec ns_1 ping -i0.01 -c 3 10.10.10.10
+ip netns exec ns_0 iperf3 -s -B 10.10.10.10 -p 7575 -D \
+	--logfile /tmp/iperf3.txt --forceflush
+ip netns exec ns_1 socat FILE:/dev/null TCP4-CONNECT:10.10.10.10:7575,retry=10
+ip netns exec ns_1 iperf3 -c 10.10.10.10 -t 3 -p 7575
+cat /tmp/iperf3.txt
+killall iperf3
+
+grep ERR /var/log/openvswitch/* || true
+ovs-vsctl show
+ovs-appctl dpif-netdev/pmd-rxq-show
+ovs-appctl dpif-netdev/pmd-stats-show
+ovs-vsctl list interface dtap0
+ovs-vsctl list interface dtap1
+ovs-vsctl list interface br-phy-0
+ip addr show
+
+test_dir=$(dirname "$0")
+"${test_dir}"/../openstack-network-exporter -l csv > "${test_dir}"/stats.csv
---
+++ test/env_test.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+
+start_iperf_server() {
+	ns="$1"
+	ip="$2"
+	ip netns exec "$ns" iperf3 -s -B "$ip" -p 7575 \
+		-D --logfile /tmp/iperf3.txt --forceflush
+	ip netns exec "$ns" socat FILE:/dev/null TCP4-CONNECT:"$ip":7575,retry=10
+	echo "iperf server is running"
+}
+
+stop_iperf() {
+	ns="$1"
+        ip netns exec "$ns" killall -9 iperf3 1>&2 2>/dev/null
+}
+
+start_iperf_client() {
+	ns="$1"
+	ip="$2"
+	duration="$3"
+	if ! [ "$duration" = 0 ]; then
+		ip netns exec "$ns" iperf3 -c "$ip" -t "$duration" -p 7575
+	fi
+}
+
+check_threshold() {
+	field=$1
+	value1=$2
+	value2=$3
+	threshold=$4
+	retvalue=0
+
+	if ! [ "$val1" = 0 ] && ! [ "$val2" = 0 ]; then
+		diff=$(awk -v v1="$val1" -v v2="$val2" \
+			'BEGIN{d=(100*(v2-v1)/v1); \
+			if (d<0) d=d*(-1);printf("%2.2f\n", d)}')
+		if awk "BEGIN {exit !($diff >= $threshold)}"; then
+			echo "ERROR: $field $value1 $value2 $diff"
+			retvalue=1
+		fi
+	else
+		echo "ERROR: $field $value1 $value2"
+		retvalue=1
+	fi
+
+	return "$retvalue"
+}
+
+compare() {
+	echo "Checking that openstack-network-exporter statistics are ok"
+	file1=$1
+	file2=$2
+	threshold=$3
+
+	# Remove statistics that will not be checked
+	set --
+	# shellcheck disable=SC2013
+	for skip in $(sed -En 's/([^,]+), *\<skip_field\>.*/\1/p' "$STATS_CONF"); do
+		set -- "$@" -e "$skip"
+	done
+	echo "Filter: $*"
+	grep -vw "$@" "$file1" > "$file1.filtered"
+	grep -vw "$@" "$file2" > "$file2.filtered"
+
+        # Check that same fields have been generated
+	sed -En 's/^([a-z0-9_]+).*/\1/p' "$file1.filtered" > "$file1.fields"
+	sed -En 's/^([a-z0-9_]+).*/\1/p' "$file2.filtered" > "$file2.fields"
+	if ! diff -u "$file1.fields" "$file2.fields" >/dev/null; then
+		echo "ERROR: Statistics set is not completed, Files have different fields"
+		return 1
+	fi
+
+	# Check that values are similar (under a defined threshold)
+	retvalue=0
+
+	while read -r -u 4 line1 && read -r -u 5 line2; do
+		if [ "$line1" = "$line2" ]; then
+			continue
+		fi
+		field1="${line1% *}"
+		field2="${line2% *}"
+		val1="${line1#* }"
+		val2="${line2#* }"
+		if ! [ "$field1" = "$field2" ]; then
+			echo "ERROR: Unexpected error, fields should coincide $field1 $field2"
+			retvalue=1
+			break
+		fi
+		field_base=$(echo "$field1" | sed -En 's/^([a-z0-9_]+).*/\1/p')
+		stat_thr=$(sed -En "s/^$field_base, .*, ([0-9]+),.*/\\1/p" "$STATS_CONF")
+		if [ -n "$stat_thr" ]; then
+			echo "Set threshold $stat_thr for $field1"
+		else
+			stat_thr="$threshold"
+		fi
+		if ! check_threshold "$field1" "$val1" "$val2" "$stat_thr"; then
+			retvalue=1
+		fi
+	done 4<"$file1.filtered" 5<"$file2.filtered"
+
+	return "$retvalue"
+}
+
+get_stats() {
+	file1="$1"
+	file2="$2"
+	options="$3"
+	echo "Getting stats"
+	curl -o "$file1" http://localhost:1981/metrics 2>/dev/null
+	# shellcheck disable=SC2086
+	"${TEST_DIR}"/get_ovs_stats.sh $options >"$file2"
+	if ! [ -f "$file1" ] || ! [ -f "$file2" ]; then
+		echo "Failed to get statistics"
+		ls -ls "$file1" "$file2"
+		return 1
+	fi
+	return 0
+}
+
+restart_openstack_network_exporter() {
+	killall -9 openstack-network-exporter 2>/dev/null || true
+	"$BASE_DIR"/openstack-network-exporter &
+	sleep 5
+}
+
+get_environment() {
+	namespaces=$(ip netns ls | awk '{print $1}')
+	for ns in $namespaces; do
+		ip=$(ip netns exec "$ns" ip a | grep -v "127.0.0.1" |
+			sed -En 's/.*inet ([0-9.]+).*/\1/p')
+		echo "$ns $ip"
+	done
+}
+
+init_test() {
+	TESTNAME=$(basename "$0")
+	TESTNAME=${TESTNAME%.*}
+	TESTDIR="$LOGS_DIR/$TESTNAME"
+
+	mkdir -p "$TESTDIR"
+}
+
+end_test() {
+	if [ "$1" != 0 ]; then
+		echo "$TESTNAME: Testcase failed" |
+			tee -a "$LOGS_DIR"/testcases.log
+	else
+		echo "$TESTNAME: Testcase passed" |
+			tee -a "$LOGS_DIR"/testcases.log
+	fi
+	exit "$1"
+}
+
+ONE_CONFIG="/etc/openstack-network-exporter.yaml"
+LOGS_DIR="$(dirname "$0")/logs"
+TEST_DIR="$(dirname "$0")"
+BASE_DIR="$TEST_DIR/../"
+STATS_CONF="$TEST_DIR"/stats_conf.csv
+THRESHOLD=2
+IPERF_DURATION=10
+
+ips=$(get_environment | tr '\n' ' ')
+echo "ips      : $ips"
+
+NS_0=$(echo "$ips" | awk '{print $1}')
+IP_0=$(echo "$ips" | awk '{print $2}')
+NS_1=$(echo "$ips" | awk '{print $3}')
+IP_1=$(echo "$ips" | awk '{print $4}')
