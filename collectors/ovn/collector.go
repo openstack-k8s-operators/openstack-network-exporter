openstack-k8s-operators/openstack-network-exporter

commit 2d6afe493f05a3e5e241a9283ddd1313348acef8

collectors: add ovn-controller
Parse the output of ovn-appctl coverage/show and
ovs-vsctl get Open_vSwitch . external_ids and export appropriate
metrics for the ovn-controller.

Signed-off-by: Miguel Lavalle <mlavalle@redhat.com>

collectors/ovn/collector.go
@@ -1,0 +1,179 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2024 Miguel Lavalle
+
+package ovn
+
+import (
+	"bufio"
+	"context"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/appctl"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/lib"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/config"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/log"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/ovsdb"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/ovsdb/ovs"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func collectopenvSwitch(externaIds map[string]string, ch chan<- prometheus.Metric) {
+	for name, metric := range openvSwitch {
+		value, ok := externaIds[name]
+		if !ok {
+			continue
+		}
+		if !config.MetricSets().Has(metric.Set) {
+			continue
+		}
+
+		val, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			log.Errf("%s: %s: %s", name, value, err)
+			continue
+		}
+
+		ch <- prometheus.MustNewConstMetric(metric.Desc(), metric.ValueType, val)
+	}
+}
+
+func collectopenvSwitchBoolean(externaIds map[string]string, ch chan<- prometheus.Metric) {
+	for name, metric := range openvSwitchBoolean {
+		value, ok := externaIds[name]
+		if !ok {
+			continue
+		}
+		if !config.MetricSets().Has(metric.Set) {
+			continue
+		}
+
+		val := 1.0
+		if strings.ToLower(value) != "true" {
+			val = 0.0
+		}
+
+		ch <- prometheus.MustNewConstMetric(metric.Desc(), metric.ValueType, val)
+	}
+}
+
+func parse_mappings(bridgeMappings string) map[string]string {
+	// This function is based on the one that Neutron uses to parse bridge
+	// mappings:
+	// https://github.com/openstack/neutron-lib/blob/ef72d4cd6e2e74a0452dcea916613357c3627a22/neutron_lib/utils/helpers.py
+	mappings := make(map[string]string)
+	mappingsSlice := strings.Split(bridgeMappings, ",")
+	for _, m := range mappingsSlice {
+		mSplit := strings.Split(m, ":")
+		mappings[strings.TrimSpace(mSplit[0])] = strings.TrimSpace(mSplit[1])
+	}
+	return mappings
+}
+
+func collectopenvSwitchLabels(externaIds map[string]string, ch chan<- prometheus.Metric) {
+	value := 1.0
+	for name, metric := range openvSwitchLabels {
+		label, ok := externaIds[name]
+		if !ok {
+			continue
+		}
+		if !config.MetricSets().Has(metric.Set) {
+			continue
+		}
+
+		ch <- prometheus.MustNewConstMetric(metric.Desc(), metric.ValueType, value, []string{label}...)
+	}
+
+	extIds, ok := externaIds["ovn-bridge-mappings"]
+	if !ok {
+		return
+	}
+	if !config.MetricSets().Has(bridgeMappings.Set) {
+		return
+	}
+	for network, bridge := range parse_mappings(extIds) {
+		labels := []string{network, bridge}
+		ch <- prometheus.MustNewConstMetric(bridgeMappings.Desc(), bridgeMappings.ValueType, value, labels...)
+	}
+}
+
+func makeMetric(name, value string) prometheus.Metric {
+	m, ok := ovnController[name]
+	if !ok {
+		return nil
+	}
+	if !config.MetricSets().Has(m.Set) {
+		return nil
+	}
+
+	val, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		log.Errf("%s: %s: %s", name, value, err)
+		return nil
+	}
+
+	return prometheus.MustNewConstMetric(m.Desc(), m.ValueType, val)
+}
+
+// "vconn_sent                 0.0/sec     0.083/sec        0.0767/sec   total: 131870"
+var coverageRe = regexp.MustCompile(`^(\w+)\s+.*\s+total: (\d+)$`)
+
+func collectCoverageMetrics(ch chan<- prometheus.Metric) {
+	buf := appctl.OvnController("coverage/show")
+	if buf == "" {
+		return
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(buf))
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		match := coverageRe.FindStringSubmatch(line)
+		if match != nil {
+			metric := makeMetric(match[1], match[2])
+			if metric != nil {
+				ch <- metric
+			}
+		}
+	}
+}
+
+type Collector struct{}
+
+func (Collector) Name() string {
+	return "ovn"
+}
+
+func (Collector) Metrics() []lib.Metric {
+	var res []lib.Metric
+	for _, g := range metrics {
+		for _, m := range *g {
+			res = append(res, m)
+		}
+	}
+	return res
+}
+
+func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
+	lib.DescribeEnabledMetrics(c, ch)
+}
+
+func (Collector) Collect(ch chan<- prometheus.Metric) {
+	// collect items from the ExternalIDs field in the OpenvSwitch table
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	var vswitch ovs.OpenvSwitch
+	err := ovsdb.Get(ctx, &vswitch)
+	if err != nil {
+		log.Errf("OvsdbGet(vswitch): %s", err)
+		return
+	}
+	collectopenvSwitch(vswitch.ExternalIDs, ch)
+	collectopenvSwitchBoolean(vswitch.ExternalIDs, ch)
+	collectopenvSwitchLabels(vswitch.ExternalIDs, ch)
+
+	// collect the ovn-controller coverage metrics
+	collectCoverageMetrics(ch)
+}

collectors/collectors.go
@@ -10,6 +10,7 @@ import (
 	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/iface"
 	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/lib"
 	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/memory"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/ovn"
 	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/pmd_perf"
 	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/pmd_rxq"
 	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/vswitch"
@@ -22,6 +23,7 @@ var collectors = []lib.Collector{
 	new(datapath.Collector),
 	new(iface.Collector),
 	new(memory.Collector),
+	new(ovn.Collector),
 	new(pmd_perf.Collector),
 	new(pmd_rxq.Collector),
 	new(vswitch.Collector),

collectors/ovn/metrics.go
@@ -1,0 +1,169 @@
+package ovn
+
+import (
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/collectors/lib"
+	"github.com/openstack-k8s-operators/dataplane-node-exporter/config"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var openvSwitch = map[string]lib.Metric{
+	"ovn-remote-probe-interval": {
+		Name:        "ovnc_remote_probe_interval",
+		Description: "Maximum number of milliseconds of idle time on connection to the OVN SB DB before sending an inactivity probe message",
+		ValueType:   prometheus.GaugeValue,
+		Set:         config.METRICS_COUNTERS,
+	},
+	"ovn-openflow-probe-interval": {
+		Name:        "ovnc_openflow_probe_interval",
+		Description: "Maximum number of milliseconds of idle time on OpenFlow connection to the OVS bridge before sending an inactivity probe message",
+		ValueType:   prometheus.GaugeValue,
+		Set:         config.METRICS_COUNTERS,
+	},
+}
+
+var openvSwitchBoolean = map[string]lib.Metric{
+	"ovn-monitor-all": {
+		Name:        "ovnc_monitor_all",
+		Description: "Specifies if ovn-controller should monitor all records of tables in OVN SB DB. The value of 0 means it will conditionally monitor the records that are needed in the current chassis",
+		ValueType:   prometheus.GaugeValue,
+		Set:         config.METRICS_BASE,
+	},
+}
+
+var openvSwitchLabels = map[string]lib.Metric{
+	"ovn-encap-ip": {
+		Name:        "ovnc_encap_ip",
+		Description: "A metric with a constant '1' value labeled by ipadress that specifies the encapsulation ip address configured on that node",
+		Labels:      []string{"encap_ip"},
+		ValueType:   prometheus.GaugeValue,
+		Set:         config.METRICS_BASE,
+	},
+	"ovn-remote": {
+		Name:        "ovnc_sb_connection_method",
+		Description: "A metric with a constant '1' value labeled by sb_connection_method that specifies the ovn-remote value configured on that node",
+		Labels:      []string{"sb_connection_method"},
+		ValueType:   prometheus.GaugeValue,
+		Set:         config.METRICS_BASE,
+	},
+	"ovn-encap-type": {
+		Name:        "ovnc_encap_type",
+		Description: "A metric with a constant '1' value labeled by type that specifies the encapsulation type that a chassis should use to connect to this node",
+		Labels:      []string{"encap_type"},
+		ValueType:   prometheus.GaugeValue,
+		Set:         config.METRICS_BASE,
+	},
+}
+
+var bridgeMappings = lib.Metric{
+	Name:        "ovnc_bridge_mappings",
+	Description: "A metric with a constant '1' value labeled by mapping that specifies a list of key-value pairs that map a physical network name to a local ovs bridge that provides connectivity to that network.",
+	Labels:      []string{"network", "bridge"},
+	ValueType:   prometheus.GaugeValue,
+	Set:         config.METRICS_BASE,
+}
+
+var ovnController = map[string]lib.Metric{
+	"lflow_run": {
+		Name:        "ovnc_lflow_run",
+		Description: "Number of times ovn-controller has translated the Logical_Flow table in the OVN SB database into OpenFlow flow",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"rconn_sent": {
+		Name:        "ovnc_rconn_sent",
+		Description: "Specifies the number of messages that have been sent to the underlying virtual connection (unix, tcp, or ssl) to OpenFlow devices",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"rconn_queued": {
+		Name:        "ovnc_rconn_queued",
+		Description: "Specifies the number of messages that have been queued because it couldn’t be sent using the underlying virtual connection to OpenFlow devices",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+	"rconn_discarded": {
+		Name:        "ovnc_rconn_discarded",
+		Description: "Specifies the number of messages that have been dropped because the send queue had to be flushed because of reconnection.",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+	"rconn_overflow": {
+		Name:        "ovnc_rconn_overflow",
+		Description: "Specifies the number of messages that have been dropped because of the queue overflow",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+	"vconn_open": {
+		Name:        "ovnc_vconn_open",
+		Description: "Specifies the number of attempts to connect to an OpenFlow Device",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"vconn_sent": {
+		Name:        "ovnc_vconn_sent",
+		Description: "Specifies the number of messages sent to the OpenFlow Device",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"vconn_received": {
+		Name:        "ovnc_vconn_received",
+		Description: "Specifies the number of messages received from the OpenFlow Device",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"stream_open": {
+		Name:        "ovnc_stream_open",
+		Description: "Specifies the number of attempts to connect to a remote peer (active connection)",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"txn_success": {
+		Name:        "ovnc_txn_success",
+		Description: "Specifies the number of times the OVSDB transaction has successfully completed",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"txn_error": {
+		Name:        "ovnc_txn_error",
+		Description: "Specifies the number of times the OVSDB transaction has errored out",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+	"txn_uncommitted": {
+		Name:        "ovnc_txn_uncommitted",
+		Description: "Specifies the number of times the OVSDB transaction were uncommitted",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+	"txn_unchanged": {
+		Name:        "ovnc_txn_unchanged",
+		Description: "Specifies the number of times the OVSDB transaction resulted in no change to the database",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_DEBUG,
+	},
+	"txn_incomplete": {
+		Name:        "ovnc_txn_incomplete",
+		Description: "Specifies the number of times the OVSDB transaction did not complete and the client had to re-try",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+	"txn_aborted": {
+		Name:        "ovnc_txn_aborted",
+		Description: "Specifies the number of times the OVSDB transaction has been aborted",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+	"txn_try_again": {
+		Name:        "ovnc_txn_try_again",
+		Description: "Specifies the number of times the OVSDB transaction failed and the client had to re-try",
+		ValueType:   prometheus.CounterValue,
+		Set:         config.METRICS_ERRORS,
+	},
+}
+
+var metrics = []*map[string]lib.Metric{
+	&openvSwitch,
+	&openvSwitchBoolean,
+	&openvSwitchLabels,
+	&ovnController,
+}

commit cf0eec13b1b00be824b481e72619d9ce84d58071

collectors: add ovnc_bridge_mappings to available metrics
ovnc_bridge_mappings is not reported by openstack-network-exporter -l.
Add it to the exported metrics of the ovn collector.

Fixes: 2d6afe493f05 ("collectors: add ovn-controller")
Signed-off-by: Miguel Lavalle <mlavalle@redhat.com>

collectors/ovn/collector.go
@@ -194,6 +194,7 @@ func (Collector) Metrics() []lib.Metric {
 			res = append(res, m)
 		}
 	}
+	res = append(res, bridgeMappings)
 	return res
 }
 